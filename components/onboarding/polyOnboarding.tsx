68d9d920d3131a4ce2c353b4d79e0909988eaef8
Qianbo <pencilate@outlook.com>
2024-06-15 16:28:11 +0800

    Completed Poly Onboarding Process

--- components/onboarding/polyOnboarding.tsx
+++ components/onboarding/polyOnboarding.tsx
@@ -1,35 +1,122 @@
-import { Button, ButtonGroup } from "@nextui-org/react";
-
-import { OBPrevEducation, PolyEnum } from "../../utils/enum";
-import { subtitle } from "../primitives";
+import { Button, ButtonGroup, Select, SelectItem } from "@nextui-org/react";
 import { ArrowLeftOutlined } from "@ant-design/icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export const NonPolyOnboarding = ({ onboarding, setOnboarding }) => {
-  const [poly, setPoly] = useState(null)
-  
-  let setMathPrereq = (e) => {
-    console.log(e.target.value)
-    setOnboarding({ ...onboarding, mathPrereq: e.target.value ? true : false });
+import { subtitle } from "../primitives";
+
+export const PolyOnboarding = ({ onboarding, setOnboarding, setReady }) => {
+  const [poly, setPoly] = useState(null);
+  const [polyList, setPolyList] = useState<PolyList[]>([]);
+  const [diplomaList, setDiplomaList] = useState([]);
+
+  let setPolySelection = (e) => {
+    console.log(e.target.value);
+    setOnboarding({ ...onboarding, poly: e.target.value });
+    setPoly(e.target.value);
   };
 
   let onBack = () => {
     setOnboarding({ eduBackground: null });
-  }
+  };
+
+  let setMathPrereq = (e) => {
+    console.log(e.target.value);
+    setOnboarding({ ...onboarding, mathPrereq: e.target.value ? true : false });
+  };
+
+  let setDiplomaSelection = (e) => {
+    console.log(e.target.value);
+    setOnboarding({ ...onboarding, diploma: e.target.value });
+  };
+
+  useEffect(() => {
+    fetch("/api/onboarding/poly/")
+      .then((res) => res.json())
+      .then((data) => {
+        setPolyList(data.poly);
+        console.log(data);
+      });
+  }, [poly]);
+
+  useEffect(() => {
+    if (poly != null) {
+      fetch(`/api/onboarding/poly/${poly}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setDiplomaList(data.courses);
+          console.log(data);
+        });
+    }
+  }, [poly]);
+
+  useEffect(() => {
+    setReady(onboarding.eduBackground && onboarding.mathPrereq && onboarding.poly && onboarding.diploma)
+  },[onboarding])
 
   return (
     <section className="flex flex-col items-left justify-Left gap-4 py-8 px-8 md:py-10">
       <Button
+        isIconOnly
         color="primary"
         size="md"
         variant="shadow"
-        isIconOnly
         onPress={onBack}
       >
         <ArrowLeftOutlined />
       </Button>
       <div className="inline-block max-w-lg text-left">
-        <h1 className={subtitle()}>Please select your polytechnic course</h1>
+        <h1 className={subtitle()}>Please select your polytechnic</h1>
+      </div>
+
+      <div className="flex gap-10 w-2/5">
+        <ButtonGroup>
+          {polyList.map((p, key) => (
+            <Button
+              key={p.id}
+              color="primary"
+              fullWidth={true}
+              size="lg"
+              value={p.id}
+              variant="shadow"
+              onPress={setPolySelection}
+            >
+              {p.name}
+            </Button>
+          ))}
+        </ButtonGroup>
+      </div>
+
+      {poly != null ? (
+        <>
+          <div className="inline-block max-w-lg text-left">
+            <h1 className={subtitle()}>Please select your diploma</h1>
+          </div>
+
+          <div className="flex gap-10">
+            <Select
+              isRequired
+              className="max-w-xs"
+              color="primary"
+              label="Select your diploma"
+              placeholder="Diploma in ..."
+              size="lg"
+              onChange={setDiplomaSelection}
+            >
+              {diplomaList.map((d, key) => (
+                <SelectItem key={d.dipid}>{d.name}</SelectItem>
+              ))}
+            </Select>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+
+      <div className="inline-block max-w-lg text-left">
+        <h1 className={subtitle()}>
+          Did you have a recognised certification for math/ Pass the exemption
+          test for MA1301?
+        </h1>
       </div>
 
       <div className="flex gap-10 w-2/5">
@@ -38,34 +125,30 @@ export const NonPolyOnboarding = ({ onboarding, setOnboarding }) => {
             color="primary"
             fullWidth={true}
             size="lg"
-            value={PolyEnum.sp}
+            value={1}
             variant="shadow"
             onPress={setMathPrereq}
           >
-            SP
+            Yes
           </Button>
           <Button
+            className="text-white min-w-100"
             color="primary"
             fullWidth={true}
             size="lg"
-            value={PolyEnum.sp}
+            value={0}
             variant="shadow"
             onPress={setMathPrereq}
           >
-            SP
-          </Button>
-          <Button
-            color="primary"
-            fullWidth={true}
-            size="lg"
-            value={PolyEnum.sp}
-            variant="shadow"
-            onPress={setMathPrereq}
-          >
-            SP
+            No
           </Button>
         </ButtonGroup>
       </div>
     </section>
   );
+};
+
+type PolyList = {
+  id: string;
+  name: string;
 };
