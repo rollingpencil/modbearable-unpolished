835bc46ca1b28fa719ab00e152041752c89c5db4
Qianbo <pencilate@outlook.com>
2024-06-15 16:27:19 +0800

    Completed Previous Education Selection

--- components/onboarding/previousEducationSelection.tsx
+++ components/onboarding/previousEducationSelection.tsx
@@ -1,12 +1,22 @@
 import { Button, ButtonGroup } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 
-import { OBPrevEducation } from "../../utils/enum";
 import { subtitle } from "../primitives";
 
 export const PrevEducationSelection = ({ onboarding, setOnboarding }) => {
+  let [instituteList, setInstituteList] = useState([]);
   let setPrevEdu = (e) => {
     setOnboarding({ ...onboarding, eduBackground: e.target.value });
   };
+
+  useEffect(() => {
+    fetch("/api/onboarding/institute/")
+      .then((res) => res.json())
+      .then((data) => {
+        setInstituteList(data.institutues);
+        console.log(data);
+      });
+  }, []);
 
   return (
     <section className="flex flex-col items-left justify-Left gap-4 py-8 px-8 md:py-10">
@@ -14,40 +24,22 @@ export const PrevEducationSelection = ({ onboarding, setOnboarding }) => {
         <h1 className={subtitle()}>What is your education background?</h1>
       </div>
 
-      <div className="flex gap-10 w-2/5">
+      <div className="flex gap-10 w-4/5">
         <ButtonGroup>
-          <Button
-            color="primary"
-            fullWidth={true}
-            size="lg"
-            value={OBPrevEducation.jc}
-            variant="shadow"
-            onPress={setPrevEdu}
-          >
-            Junior College
-          </Button>
-          <Button
-            className="text-white min-w-100"
-            color="primary"
-            fullWidth={true}
-            size="lg"
-            value={OBPrevEducation.ib}
-            variant="shadow"
-            onPress={setPrevEdu}
-          >
-            Intl Baccalaureate
-          </Button>
-          <Button
-            className="text-white"
-            color="primary"
-            fullWidth={true}
-            size="lg"
-            value={OBPrevEducation.poly}
-            variant="shadow"
-            onPress={setPrevEdu}
-          >
-            Diploma
-          </Button>
+          {instituteList.map((inst, key) => (
+            <Button
+              key={inst.instituteid}
+              color="primary"
+              fullWidth={true}
+              size="lg"
+              value={inst.instituteid}
+              variant="shadow"
+              onPress={setPrevEdu}
+              className="w-100"
+            >
+              {inst.name}
+            </Button>
+          ))}
         </ButtonGroup>
       </div>
     </section>
