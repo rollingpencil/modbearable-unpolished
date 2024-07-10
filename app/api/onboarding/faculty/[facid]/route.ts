d6e6a83c82e0b4fccc30305ce9c9336adb4d3c75
Qianbo <pencilate@outlook.com>
2024-07-10 22:06:19 +0800

    Added retrieve faculty and retrieve majors API

---
+++ app/api/onboarding/faculty/[facid]/route.ts
@@ -0,0 +1,56 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+type FacIDParams = {
+  facid: string;
+};
+
+type MajorType = {
+  id: number;
+  name: string;
+}
+
+export async function GET(request: Request, context: { params: FacIDParams }) {
+  const { facid } = context.params;
+
+  try {
+    console.log("Called /faculty/[facid]")
+
+    const facIdNum = Number(facid)
+
+    await prisma.$connect();
+
+    // Fetch diplomas based on polytechnicId
+    const facultyMajors : MajorType[] = await prisma.major.findMany({
+      where: {
+        facultyId: facIdNum,
+      },
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        majors:
+            facultyMajors.length > 0
+            ? facultyMajors.map((major: MajorType) => ({
+                majorid: major.id,
+                name: major.name,
+              }))
+            : [],
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Error fetching majors", error);
+
+    return NextResponse.json(
+      { error: "Failed to fetch data from database." },
+      { status: 500 },
+    );
+  }
+}
---
+++ app/api/onboarding/faculty/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(request: Request) {
+  try {
+    console.log("Called /faculty")
+    await prisma.$connect();
+    // Fetch polytechnic data from the database
+    const faculty = await prisma.faculty.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+
+    // Return the fetched data as JSON
+    return NextResponse.json({ faculties: faculty }, { status: 200 });
+  } catch (error) {
+    // Handle potential errors
+    console.error("Failed to fetch faculty:", error);
+
+    return NextResponse.json(
+      { error: "Failed to fetch data from database." },
+      { status: 500 },
+    );
+  }
+}
