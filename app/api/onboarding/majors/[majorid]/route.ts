5dec5fc30c42b300a10c7ce040ed26b1e3b52aa9
Qianbo <pencilate@outlook.com>
2024-07-11 16:01:59 +0800

    Added get cohort api

---
+++ app/api/onboarding/majors/[majorid]/route.ts
@@ -0,0 +1,59 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+type MajorIDParams = {
+  majorid: string;
+};
+
+type CohortType = {
+  id: number;
+  name: string;
+};
+
+export async function GET(
+  request: Request,
+  context: { params: MajorIDParams },
+) {
+  const { majorid } = context.params;
+
+  try {
+    console.log("Called /majors/[majorid]");
+
+    const majorIdNum = Number(majorid);
+
+    await prisma.$connect();
+
+    // Fetch diplomas based on polytechnicId
+    const facultyMajorCohort: CohortType[] = await prisma.cohort.findMany({
+      where: {
+        majorId: majorIdNum,
+      },
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        cohorts:
+          facultyMajorCohort.length > 0
+            ? facultyMajorCohort.map((cohort: CohortType) => ({
+                cohortid: cohort.id,
+                name: cohort.name,
+              }))
+            : [],
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Error fetching cohort", error);
+
+    return NextResponse.json(
+      { error: "Failed to fetch data from database." },
+      { status: 500 },
+    );
+  }
+}
