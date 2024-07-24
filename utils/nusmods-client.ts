3c8a44b8073274afa41f6c87c6d6829d61f7fec3
Qianbo <pencilate@outlook.com>
2024-07-24 12:18:54 +0800

    remoce dependancy from axios

--- utils/nusmods-client.ts
+++ utils/nusmods-client.ts
@@ -1,21 +1,25 @@
-import axios from "axios";
+export const retrieveSpecificMods = async (
+  cohort: string,
+  courseCode: string,
+  wildcard: boolean,
+) => {
+  let baseResponse = {
+    // Return a default or empty object, or handle it according to your requirements
+    prereqTree: null,
+    semesterData: [],
+    fulfillRequirements: [],
+  };
 
-export const retrieveSpecificMods = async (cohort: string, courseCode: string,wildcard: boolean) => {
-
-    if (wildcard == true || courseCode.startsWith("EX")){
+  if (wildcard == true || courseCode.startsWith("EX")) {
     // Handle the case where wildcard is true
-    return {
-        // Return a default or empty object, or handle it according to your requirements
-        prereqTree: null,
-        semesterData: [],
-        fulfillRequirements: [],
-    };
-    } else {
-        const response = await axios.get(
-            `https://api.nusmods.com/v2/${cohort}/modules/${courseCode}.json`,
-        );
-        return response.data;
-         
-    }
-    
-}
+    return baseResponse;
+  } else {
+    fetch(`https://api.nusmods.com/v2/${cohort}/modules/${courseCode}.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        baseResponse = data;
+      });
+
+    return baseResponse;
+  }
+};
