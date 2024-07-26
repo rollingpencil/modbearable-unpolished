406a0576fd1439c99b5d3c8cdb6d42b070db8fef
Qianbo <pencilate@outlook.com>
2024-07-26 16:53:58 +0800

    Remove gap from planner layout

--- app/planner/[[...importStr]]/layout.tsx
+++ app/planner/[[...importStr]]/layout.tsx
@@ -4,7 +4,7 @@ export default function PlannerLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 h-full">
+    <section className="flex flex-col items-center justify-center gap-4 h-full">
       {children}
     </section>
   );
